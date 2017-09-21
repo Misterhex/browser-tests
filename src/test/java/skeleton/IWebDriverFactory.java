234baadd556d054fc7610afbe55ea8368483675f
Yong Heng <yongheng@live.com.sg>
2017-09-21 14:25:12 +0800

    fix build and add drone yml

--- src/test/java/skeleton/IWebDriverFactory.java
+++ src/test/java/skeleton/IWebDriverFactory.java
@@ -12,11 +12,3 @@ public interface IWebDriverFactory {
     WebDriver Create() throws MalformedURLException;
 }
 
-public class WebDriverFactory implements IWebDriverFactory {
-
-    @Override
-    public WebDriver Create() throws MalformedURLException {
-        RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), DesiredCapabilities.chrome());
-
-    }
-}
--- src/test/java/skeleton/Login.java
+++ src/test/java/skeleton/Login.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.Given;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Login {
@@ -11,8 +12,11 @@ public class Login {
     private final WebDriver driver = new WebDriverFactory().Create();
     private final URL AcqoUrl = new URL("http://dev.acuo.com");
 
+    public Login() throws MalformedURLException {
+    }
+
     @Given("^I am on the login page$")
-    public void I am_on_the_login_page() throws Throwable {
+    public void I_am_on_the_login_page() throws Throwable {
 
         driver.get(AcqoUrl.toString());
         driver.findElement(By.cssSelector("form div.Login__input___3nbxz input[type='text']"));
